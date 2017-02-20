926e7d908d8329ac6f89ea57d5316877a1dcf3b9
pape-one <pitu96_drn23@hotmail.com>
2017-02-20 11:11:01 +0100

    Class pizza

---
+++ HashCodePractice/src/hashcodepractice/Pizza.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import java.awt.Point;
+
+public class Pizza{
+    Cell[][] piz;
+    int columnas, filas, minL, maxH;
+    public Pizza(File f){
+        Scanner entrada = null;
+        try{
+            entrada = new Scanner(f);
+            filas = entrada.nextInt();
+            columnas = entrada.nextInt();
+            minL = entrada.nextInt();
+            maxH = entrada.nextInt();
+            piz = new Cell[filas][columnas];
+            int j = 0;
+            String s = "";
+            while(entrada.hasNext()){
+                s = entrada.nextLine();
+                for(int i = 0; i<s.lenght; i++){
+                   if(s.charAt(i) == 'T'){
+                       piz[j]][i] = new Cell(true, new Point(j,i));
+                   }else{
+                       piz[j]][i] = new Cell(false, new Point(j,i));
+                   }
+                   j++;
+                }
+            }
+        }catch(FileNotFoundException e){
+            System.out.println("El fichero no se puede abrir.");
+        }finally{
+            if(entrada!=null){
+                entrada.close();
+            }
+        }
+    }
+
+    public pizza getPizza(){
+        return this.piz;
+    }
+    public int getFilas(){
+        return this.filas;
+    }
+    public int getColumnas(){
+        return this.columnas;
+    }
+    public int getMinL(){
+        return this.minL;
+    }
+    public int getMaxH(){
+        return this.maxH;
+    }
+}
