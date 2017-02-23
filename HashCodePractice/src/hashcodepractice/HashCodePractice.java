c2587d9f82ed46031ee5a21b51bd43e7e4b112c6
Josep <Josep@priv2g56-154.wi-fi.upv.es>
2017-02-23 13:48:32 +0100

    Se han hecho muchas cosas, sigue sin ir, pero da atisbos

--- HashCodePractice/src/hashcodepractice/HashCodePractice.java
+++ HashCodePractice/src/hashcodepractice/HashCodePractice.java
@@ -15,7 +15,7 @@ public class HashCodePractice {
     
     private static ArrayList slices;
     
-    public void firstSlices(Pizza pizza){
+    public static void firstSlices(Pizza pizza){
         //Es necesario recorrer cada Cell de la Pizza para ver si es factible crear un Slice
         for(int nFil=0; nFil<pizza.getNumFilas(); nFil++){
             for(int nCol=0; nCol<pizza.getNumColumnas(); nCol++){
@@ -31,7 +31,7 @@ public class HashCodePractice {
                         current = new Slice(puntoIni,puntoIni,puntoIni,puntoIni,1,0);
                         pizza.piz[nFil][nCol].setSlice(true);
                     }
-                    
+                    System.out.println(current.getLUp().toString() +""+ current.getRDown().toString());
                     //Comprobaciones para evitar que se quede iterando la expansion
                     //boolean stopIzq=false,stopDer=false,stopArriba=false,stopAbajo=false;
                     
@@ -41,71 +41,102 @@ public class HashCodePractice {
                         //Comprobaciones para si es posible expandir hacia alguna direccion
                         boolean izqAvail=true,derAvail=true,arribaAvail=true,abajoAvail=true;
                         
+                        System.out.println(current.getLUp().toString() +""+ current.getRDown().toString());
                         
                         //Comprobacion de la derecha
                         int column = current.getRUp().y;
                         if(column<pizza.getNumColumnas()-1){
                             for(int pos=current.getRUp().x; pos<=current.getRDown().x; pos++){
                                 if(pizza.piz[pos][column].getIngrediente()!= pizza.piz[pos][column+1].getIngrediente()){
-                                    derSI = true;
+                                    derSI = derSI||true;
                                 }
                                 if(pizza.piz[pos][column+1].isInSlice()){
-                                    derAvail = false;
+                                    derAvail = derAvail&&false;
                                 }
                             }
+                        }else{
+                           derAvail = derAvail&&false;
                         }
-                        
+                        System.out.print("Derecha ");
                         //Comprobacion de abajo
                         int fila = current.getLDown().x;
                         if(fila<pizza.getNumFilas()-1){
                             for(int pos = current.getLDown().y; pos<=current.getRDown().y; pos++){
                                 if(pizza.piz[fila][pos].getIngrediente() != pizza.piz[fila+1][pos].getIngrediente()){
-                                    abajoSI = true;
+                                    abajoSI = abajoSI||true;
                                 }
                                 if(pizza.piz[fila+1][pos].isInSlice()){
-                                    abajoAvail = false;
+                                    abajoAvail = abajoAvail&&false;
                                 }
                             }
+                        }else{
+                            abajoAvail = abajoAvail&&false;
                         }
-                        
+                        System.out.print("Abajo ");
                         //Comprobacion de la izquierda
                         column = current.getLUp().y;
                         if(column>0){
                             for(int pos=current.getLUp().x; pos<=current.getLDown().x; pos++){
                                 if(pizza.piz[pos][column].getIngrediente() != pizza.piz[pos][column-1].getIngrediente()){
-                                    izqSI = true;
+                                    izqSI = izqSI||true;
                                 }
                                 if(pizza.piz[pos][column-1].isInSlice()){
-                                    izqAvail = false;
+                                    izqAvail = izqAvail&&false;
                                 }
                             }
+                        }else{
+                            izqAvail = izqAvail&&false;
                         }
-                        
+                        System.out.print("Izquierda ");
                         //Comprobacion de arriba
                         fila = current.getLUp().x;
                         if(fila>0){
                             for(int pos=current.getLUp().y; pos<=current.getRUp().y; pos++){
                                 if(pizza.piz[fila][pos].getIngrediente() != pizza.piz[fila-1][pos].getIngrediente()){
-                                    arribaSI = true;
+                                    arribaSI = arribaSI||true;
                                 }
                                 if(pizza.piz[fila-1][pos].isInSlice()){
-                                    arribaAvail = false;
+                                    arribaAvail = arribaAvail&&false;
                                 }
                             }
+                        }else{
+                            arribaAvail = arribaAvail&&false;
                         }
-                        
-                        if(!derAvail || !abajoAvail || !izqAvail || !arribaAvail){
+                        System.out.println("Arriba ");
+                        System.out.println(derSI + ","+ abajoSI +","+ izqSI + ","+ arribaSI);
+                        System.out.println(derAvail + ","+ abajoAvail + ","+ izqAvail + ","+ arribaAvail);
+                        if(!(arribaAvail|derAvail|abajoAvail|izqAvail)){
                             break;
                         }
-                        
+                        System.out.println("Se pasa");
                         if(derSI && derAvail){
                             //Expandir a la derecha
+                            current.setRUp(new Point(current.getRUp().x+1,current.getRUp().y));
+                            current.setRDown(new Point(current.getRDown().x+1,current.getRDown().y));
+                            for(int locCel=current.getRUp().y; locCel<=current.getRDown().y;locCel++){
+                                pizza.piz[current.getRUp().x][locCel].setSlice(true);
+                            }
                         }else if(abajoSI && abajoAvail){
                             //Expandir abajo
+                            current.setLDown(new Point(current.getLDown().x,current.getLDown().y+1));
+                            current.setRDown(new Point(current.getRDown().x,current.getRDown().y+1));
+                            for(int locCel=current.getLDown().x;locCel<=current.getRDown().x;locCel++){
+                                pizza.piz[locCel][current.getLDown().y].setSlice(true);
+                            }
                         }else if(izqSI && izqAvail){
                             //Expandir a la izquierda
+                            current.setLUp(new Point(current.getLUp().x-1,current.getLUp().y));
+                            current.setLDown(new Point(current.getLDown().x-1,current.getLDown().y));
+                            for(int locCel=current.getLUp().y;locCel<=current.getLDown().y;locCel++){
+                                pizza.piz[current.getLUp().x][locCel].setSlice(true);
+                            }
                         }else if(arribaSI && arribaAvail){
                             //Expandir arriba
+                            current.setLUp(new Point(current.getLUp().x,current.getLUp().y-1));
+                            current.setRUp(new Point(current.getRUp().x,current.getRUp().y-1));
+                            for(int locCel=current.getLUp().x;locCel<=current.getRUp().x;locCel++){
+                                pizza.piz[locCel][current.getRUp().y].setSlice(true);
+                            }
                         }else{
                             //Expandir por historial
                             switch (current.getLMovement()){
@@ -114,6 +145,11 @@ public class HashCodePractice {
                                     //El ultimo movimiento ha sido hacia arriba o es el movimiento inicial
                                     if(derAvail){
                                         //Expandir a la derecha
+                                        current.setRUp(new Point(current.getRUp().x+1,current.getRUp().y));
+                                        current.setRDown(new Point(current.getRDown().x+1,current.getRDown().y));
+                                        for(int locCel=current.getRUp().x; locCel<=current.getRDown().x;locCel++){
+                                            pizza.piz[locCel][current.getRUp().x].setSlice(true);
+                                        }
                                     }
                                     current.setLMovement(1);
                                     break;
@@ -121,6 +157,11 @@ public class HashCodePractice {
                                     //El ultimo movimiento ha sido hacia la derecha
                                     if(abajoAvail){
                                         //Expandir abajo
+                                        current.setLDown(new Point(current.getLDown().x,current.getLDown().y+1));
+                                        current.setRDown(new Point(current.getRDown().x,current.getRDown().y+1));
+                                        for(int locCel=current.getLDown().y;locCel<=current.getRDown().y;locCel++){
+                                            pizza.piz[current.getLDown().y][locCel].setSlice(true);
+                                        }
                                     }
                                     current.setLMovement(2);
                                     break;
@@ -128,6 +169,12 @@ public class HashCodePractice {
                                     //El ultimo movimiento ha sido hacia abajo
                                     if(izqAvail){
                                         //Expandir a la izquierda
+                                        System.out.println("Izquierda mov: x=" + current.getLUp().x);
+                                        current.setLUp(new Point(current.getLUp().x-1,current.getLUp().y));
+                                        current.setLDown(new Point(current.getLDown().x-1,current.getLDown().y));
+                                        for(int locCel=current.getLUp().x;locCel<=current.getLDown().x;locCel++){
+                                            pizza.piz[locCel][current.getLUp().x].setSlice(true);
+                                        }
                                     }
                                     current.setLMovement(3);
                                     break;
@@ -135,13 +182,32 @@ public class HashCodePractice {
                                     //El ultimo movimiento ha sido hacia la izquierda
                                     if(arribaAvail){
                                         //Expandir arriba
+                                        current.setLUp(new Point(current.getLUp().x,current.getLUp().y-1));
+                                        current.setRUp(new Point(current.getRUp().x,current.getRUp().y-1));
+                                        for(int locCel=current.getLUp().y;locCel<=current.getRUp().y;locCel++){
+                                            pizza.piz[current.getRUp().y][locCel].setSlice(true);
+                                        }
                                     }
                                     current.setLMovement(0);
                                     break;
                             }
+                        }   
+                    }
+                    //Acaba el while
+                    System.out.println(current.getLUp().toString() +""+ current.getRDown().toString());
+
+                    if(!current.isValid(pizza.getMinL())){
+                        //Deseleccionar las celdas 
+                        System.out.println("Un slice no es valido");
+                        for(int xLoc=current.getLUp().x;xLoc<=current.getLDown().x;xLoc++){
+                            for(int yLoc=current.getLUp().y;yLoc<=current.getRUp().y;yLoc++){
+                                pizza.piz[xLoc][yLoc].setSlice(false);
+                            }
                         }
-                        
-                        
+                    }else{
+                        //Añadir al array del slices
+                        System.out.println("Se añade un slice");
+                        slices.add(current);
                     }
                 }
             }
@@ -160,86 +226,8 @@ public class HashCodePractice {
             Pizza pizza = new Pizza(fich);
             slices = new ArrayList();
             
-            int j=0;
-            for(int i = 0; i< pizza.getPizza()[0].length; i++){
-                Point aux = new Point(j,i);
-                Slice current = null;
-                if(pizza.getPizza()[j][i].getIngrediente()){
-                    current = new Slice(aux,aux,aux,aux,0,1);
-                    pizza.getPizza()[j][i].setSlice(true);
-                }else{
-                    current = new Slice(aux,aux,aux,aux,1,0);
-                    pizza.getPizza()[j][i].setSlice(true);
-                }
-                
-                while(!current.isValid(pizza.getMinL())){
-                    switch(current.getLMovement()){
-                        case -1:
-                        case 3:
-                            if(current.getRDown().x < pizza.getPizza()[0].length - 1){
-                                int vLength = current.getRDown().x - current.getRUp().x;
-                                boolean free = true;
-                                boolean otherIng = false;
-                                for(int k = 0; k<= vLength; k++){
-                                    Cell currentCell = (pizza.getPizza()[current.getRUp().x + k][current.getRUp().y + 1]);
-                                    boolean inSlice = currentCell.isInSlice();
-                                    boolean ingr = currentCell.getIngrediente();
-                                    otherIng = otherIng | ((pizza.getPizza()[current.getRUp().x + k][current.getRUp().y])
-                                                            .getIngrediente() != ingr );
-                                    free = free & !inSlice;
-                                }
-                                if(free & otherIng){
-                                    for(int k = 0; k<= vLength; k++){
-                                        Cell currentCell = (pizza.getPizza()[current.getRUp().x + k][current.getRUp().y + 1]);
-                                        currentCell.setSlice(true);
-                                        if(currentCell.getIngrediente()){
-                                            current.setNTomatoe(current.getNTomatoe() + 1);
-                                        }else{
-                                            current.setNMushroom(current.getNMushroom() + 1);
-                                        }                                        
-                                    }                                    
-                                    current.setRUp(new Point (current.getRUp().x,current.getRUp().y + 1));
-                                    current.setRDown(new Point (current.getRDown().x,current.getRDown().y + 1));
-                                }
-                            }
-                            current.setLMovement(0);
-                            break;
-                        case 0:
-                            if(current.getRDown().y < pizza.getPizza().length - 1){
-                                int hLength = current.getLDown().y - current.getRDown().y;
-                                boolean free = true;
-                                boolean otherIng = false;
-                                for(int k = 0; k<= hLength; k++){
-                                    Cell currentCell = (pizza.getPizza()[current.getLDown().x + 1][current.getLDown().y + k]);
-                                    boolean inSlice = currentCell.isInSlice();
-                                    boolean ingr = currentCell.getIngrediente();
-                                    otherIng = otherIng | ((pizza.getPizza()[current.getLDown().x][current.getLDown().y + k])
-                                                            .getIngrediente() != ingr );
-                                    free = free & !inSlice;
-                                }
-                                if(free & otherIng){
-                                    for(int k = 0; k<= hLength; k++){
-                                        Cell currentCell = (pizza.getPizza()[current.getLDown().x + 1][current.getLDown().y + k]);
-                                        currentCell.setSlice(true);
-                                        if(currentCell.getIngrediente()){
-                                            current.setNTomatoe(current.getNTomatoe() + 1);
-                                        }else{
-                                            current.setNMushroom(current.getNMushroom() + 1);
-                                        }                                        
-                                    }                                    
-                                    current.setLDown(new Point (current.getLDown().x+1,current.getLDown().y));
-                                    current.setRDown(new Point (current.getRDown().x+1 ,current.getRDown().y));
-                                }
-                            }
-                            current.setLMovement(1);
-                            break;
-                        case 1:
-                        case 2:
-                           
-                    
-                    }
-                }                
-            }
+            firstSlices(pizza);
+            System.out.println(slices.toString());
         }
     }
     
